fc90a107a2fbd55ad8511b95bf3873de858ece92
Jon White <jon.white2@gmail.com>
2018-04-05 00:39:59 -0400

    created object for functions
    All functions are now stored inside the actions object

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# liri-node-app
+# LIRI-Node-App
 
 ## Purpose
 
@@ -6,21 +6,32 @@ LIRI is a Language Intrepretation and Recognition Interface that takes in a comm
 
 ## Getting Started
 
-$ node liri.js [command] [query]
+To use this app you will need your own set of API keys for the Twitter and Spotify APIs.
+
+$ node liri.js '<enter command here>' '<enter query here>'
 
 Example: $ node liri.js movie-this "Black Panther"
 
 ## Commands
 
-* my-tweets: - Displays your last 20 tweets and when they were created.
-* spotify-this-song: - Displays detailed information about a given song.
-* movie-this: - Displays detailed information about a given movie.
-* do-what-it-says: Reads a command and query from a file and executes the command
+* my-tweets: - Displays the last 20 tweets and when they were created of the specified Twitter handle. If no handle is indicated, it will display tweets from my account (BlackPanther653).
+  * Usage example 1: $ node liri.js my-tweets BarackObama
+  * Usage example 2: $ node liri.js my-tweets
+* spotify-this-song: - Displays detailed information about a given song. If no song is specified, it will default to "The Sign" by Ace of Base.
+  * Usage example 1: $ node liri.js spotify-this-song "All The Stars by Kendrick Lamar"
+  * Usage example 2: $ node liri.js spotify-this-song
+* movie-this: - Displays detailed information about a specified movie. If no movie is specified, it will default to the movie "Mr. Nobody".
+  * Usage example 1: $ node liri.js movie-this "Black Panther"
+  * Usage example 2: $ node liri.js movie-this
+* do-what-it-says: Reads a command and query from a specified text file and executes the command. If no text file is specified, it will default to random.txt. The file should be stored in the root folder.
+  * Usage example 1: $ node liri.js do-what-it-says "fileWithRandomCommand.txt"
+  * Usage example 2: $ node liri.js do-what-it-says
 
-### APIs Used
-* Twitter
-* Spotify
-* OMDB
+## APIs Used
+
+* [Twitter](https://developer.twitter.com/en/docs)
+* [Spotify](https://beta.developer.spotify.com/documentation/web-api/)
+* [OMDB](http://www.omdbapi.com/)
 
 ## Node Packages Used
 
@@ -28,9 +39,9 @@ Example: $ node liri.js movie-this "Black Panther"
 * https://www.npmjs.com/package/node-spotify-api
 * https://www.npmjs.com/package/request
 * https://www.npmjs.com/package/dotenv
-* FileSystem (fs)
+* [FileSystem (fs)](https://nodejs.org/dist/latest-v8.x/docs/api/fs.html)
 
 ## Author
 
 This project is maintained and written by 
-[Jon White](https://motionswing.github.io/)
+[Jon White](https://jonathan-white.github.io/)
--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 // Create variable to store imported keys info
 const keys = require("./keys.js");
 
-// Create variables for each of the used Node packages
+// Create variables for each of the invoked Node packages
 const Spotify = require('node-spotify-api');
 const Twitter = require('twitter');
 const request = require('request');
@@ -14,37 +14,31 @@ const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 const omdb = keys.omdb.api_key || 'trilogy';
 
-runCommand(process.argv[2], process.argv[3]);
-
-// Takes in two parameters (command & query). 
-// Command = a command (my-tweets, spotify-this-song, movie-this, or do-what-it-says)
-// Query = input for the command
-function runCommand(command, query){
-	if (command === "my-tweets") {
-		console.log('Let me pull your tweets...');
-		logData('\r\n-----------------');
-		logData('Let me pull your tweets...');
-		client.get('statuses/user_timeline',{screen_name: 'BlackPanther653',count: 20}, (error, tweet, response) => {
+// Store all functions in the 'actions' object
+var actions = {
+	// The twitter function calls the Twitter API and returns the selected user's last 20 tweets
+	twitter: (handle) => {
+		client.get('statuses/user_timeline',{screen_name: handle,count: 20}, (error, tweet, response) => {
 			if(!error){
+				// Loop through and display/log returned tweets 
 				for (var i = 0; i < tweet.length; i++) {
 					console.log('['+tweet[i].created_at + '] ' + tweet[i].text);
-					logData('['+tweet[i].created_at + '] ' + tweet[i].text);
+					actions.writeToLogFile('['+tweet[i].created_at + '] ' + tweet[i].text);
 				}
 			}else {
-				logData('Error: ' + error);
-				return console.log('Error: ' + error);
+				// If there is an error display/log the error message
+				actions.writeToLogFile('Error occurred: ' + error);
+				return console.log('Error occurred: ' + error);
 			}
 		});
-	} else if (command === "spotify-this-song") {
-		query = query || 'The Sign Ace of Base';
-		console.log('Searching for the song '+ query +'...');
-		logData('\r\n-----------------');
-		logData('Searching for the song '+ query +'...');
+	},
 
-		// Search for the song
-		spotify.search({ type: 'track', query: query, limit: 1 }, (err, data) => {
+	// The spotify function searches the Spotify API for the specified song
+	// Prints/logs details about the song including artist(s), song name, preview link & album 
+	spotify: (song) => {
+		spotify.search({ type: 'track', query: song, limit: 1 }, (err, data) => {
 			if (err) {
-				logData('Error occurred: ' + err);
+				actions.writeToLogFile('Error occurred: ' + err);
 				return console.log('Error occurred: ' + err);
 			}
 			const result = data.tracks.items;
@@ -59,7 +53,7 @@ function runCommand(command, query){
 				// Display list of artists
 				for (var r = 0; r < artists.length; r++) {
 					console.log('Artist['+ (r + 1)+']: '+ artists[r].name);
-					logData('Artist['+ (r + 1)+']: '+ artists[r].name);
+					actions.writeToLogFile('Artist['+ (r + 1)+']: '+ artists[r].name);
 				}
 
 				// Display data in console
@@ -68,21 +62,19 @@ function runCommand(command, query){
 				console.log('Album ('+ albumType +'): ' + album);
 
 				// Write data to log file
-				logData('Song name: ' + song);
-				logData('Preview Link: ' + link);
-				logData('Album ('+ albumType +'): ' + album);
+				actions.writeToLogFile('Song name: ' + song);
+				actions.writeToLogFile('Preview Link: ' + link);
+				actions.writeToLogFile('Album ('+ albumType +'): ' + album);
 			}
 		});
-	} else if (command === "movie-this") {
-		query = query || 'Mr Nobody';
-		console.log('Searching for the movie "'+ query +'"...');
-		logData('\r\n-----------------');
-		logData('Searching for the movie "'+ query +'"...');
+	},
 
-		// Search for the movie
-		request('http://www.omdbapi.com/?t='+ encodeURIComponent(query) +'&apikey=' + omdb, (error, response, body) => {
+	// The omdb function searches the OMDB API for the specified movie
+	// Prints/logs details about the movie
+	omdb: (movie) => {
+		request('http://www.omdbapi.com/?t='+ encodeURIComponent(movie) +'&apikey=' + omdb, (error, response, body) => {
 			if (error) {
-				logData('Error occurred: ' + error);
+				actions.writeToLogFile('Error occurred: ' + error);
 				return console.log('Error occurred: ' + error);
 			}
 			if(body){
@@ -93,51 +85,100 @@ function runCommand(command, query){
 				console.log('Title: '+result.Title);
 			  	console.log('Year: '+result.Year);
 			  	console.log('IMDB Rating: '+result.Ratings[0].Value);
-			  	console.log('Rotten Tomatoes Rating: '+result.Ratings[1].Value);
+			  	if(result.Ratings[1]){
+			  		console.log('Rotten Tomatoes Rating: '+result.Ratings[1].Value);
+			  	}
 			  	console.log('Country: '+result.Country);
 			  	console.log('Language: '+result.Language);
 			  	console.log('Plot: '+result.Plot);
 			  	console.log('Actor(s): '+result.Actors);
 
 			  	// Write data to log file
-			  	logData('Title: '+result.Title);
-			  	logData('Year: '+result.Year);
-			  	logData('IMDB Rating: '+result.Ratings[0].Value);
-			  	logData('Rotten Tomatoes Rating: '+result.Ratings[1].Value);
-			  	logData('Country: '+result.Country);
-			  	logData('Language: '+result.Language);
-			  	logData('Plot: '+result.Plot);
-			  	logData('Actor(s): '+result.Actors);
+			  	actions.writeToLogFile('Title: '+result.Title);
+			  	actions.writeToLogFile('Year: '+result.Year);
+			  	actions.writeToLogFile('IMDB Rating: '+result.Ratings[0].Value);
+			  	if(result.Ratings[1]){
+			  		actions.writeToLogFile('Rotten Tomatoes Rating: '+result.Ratings[1].Value);
+				}
+			  	actions.writeToLogFile('Country: '+result.Country);
+			  	actions.writeToLogFile('Language: '+result.Language);
+			  	actions.writeToLogFile('Plot: '+result.Plot);
+			  	actions.writeToLogFile('Actor(s): '+result.Actors);
 			}
 		});
+	},
 
-	} else if (command === "do-what-it-says") {
+	// The random function executes a command from the random.txt file
+	random: (file) => {
 		// Read data from the file
-		fs.readFile('random.txt', 'utf8', (err, data) => {
-		  if (err) throw err;
+		fs.readFile(file, 'utf8', (err, data) => {
+			if (err) throw err;
 
-		  // Convert data Buffer to a string
-		  var fileData = data.split(',');
+			// Convert data Buffer to a string
+			var fileData = data.split(',');
 
-		  // Extract the command from the file
-		  var fCommand = fileData[0];
+			// Extract the command from the file
+			var fileCommand = fileData[0];
 
-		  // Extract the query from the file
-		  var fQuery = fileData[1];
+			// Extract the query from the file
+			// Remove quotes from query string 
+			var fileQuery = fileData[1].replace(/\"/g,'');
 
-		  // Use recursion to run the command from the file
-		  runCommand(fCommand, fQuery);
+			// Use recursion to run the command from the file
+			if (!(fileCommand === "do-what-it-says")){
+				actions.execute(fileCommand, fileQuery);
+			}else {
+				actions.invalidCommand("The 'do-what-it-says' command is already running. Choose another command.");
+			}
 
 		});
-	} else {
-		console.log('Sorry, "'+ command +'" is not a valid command.');
-		logData('\r\n-----------------');
-		logData('Sorry, "'+ command +'" is not a valid command.');
+	},
+
+	// The execute function takes in command and query parameters. Based on the chosen command,
+	// a different API is called using the callAPI function 
+	// Use default query values if user does not specify a value
+	execute: (command, query) => {
+		if (command === "my-tweets") {
+			query = query || 'BlackPanther653';
+			actions.callAPI('twitter','Fetching the last 20 tweets from "'+ query +'"...',query);
+		} else if (command === "spotify-this-song") {
+			query = query || 'The Sign Ace of Base';
+			actions.callAPI('spotify','Searching for the song "'+ query +'"...',query);
+		} else if (command === "movie-this") {
+			query = query || 'Mr Nobody';
+			actions.callAPI('omdb','Searching for the movie "'+ query +'"...',query);
+		} else if (command === "do-what-it-says") {
+			query = query || 'random.txt';
+			actions.callAPI('random','Pulling command from "'+ query +'" file...',query);
+		} else {
+			actions.invalidCommand('Sorry, "'+ command +'" is not a valid command.');
+		}		
+	},
+
+	// The callAPI function takes in three parameters (command, message and query)
+	// This function both consoles & writes to a log file the specified message 
+	// Additionally, based on the entered command, the associated API function is called 
+	callAPI: (command,message,query) => {
+		console.log(message);
+		actions.writeToLogFile('\r\n-----------------');
+		actions.writeToLogFile(message);
+		actions[command](query);
+	},
+
+	// The writeToLogFile function writes the specified content to the log.txt file
+	writeToLogFile: (content) => {
+		fs.appendFile('log.txt', content + '\r\n', (err) => {
+			if (err) throw err;
+		});	
+	},
+
+	// Notify user the command they entered was invalid
+	invalidCommand: (message) => {
+		console.log(message);
+		actions.writeToLogFile('\r\n-----------------');
+		actions.writeToLogFile(message);
 	}
 };
 
-function logData(data){
-	fs.appendFile('log.txt', data + '\r\n', (err) => {
-	  if (err) throw err;
-	});	
-};
+// Start the app!
+actions.execute(process.argv[2], process.argv[3]);
